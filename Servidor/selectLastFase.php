c05db8d1506a03a22bf8151ceff327d10d40ad25
Raphael Sousa <Raphael Sousa>
2017-01-25 13:09:51 -0200

    novos php para pegar ultima fase e pergunta

---
+++ Servidor/selectLastFase.php
@@ -0,0 +1,24 @@
+<?php
+    include("conexao.php");
+
+    $select = "SELECT * FROM fase ORDER BY id DESC LIMIT 1";
+    
+    $result = $conexao->prepare($select);
+    $result->execute();
+    $count = $result->rowCount(); 
+
+    if($count > 0) {
+        echo 1;
+        foreach($result as $fases) {
+            $id = $fases['id'];
+            $nome = $fases['nome'];
+            $autor = $fases['autor'];
+            $data = $fases['data'];
+
+            echo '&id='.$id.'#'.$nome.'#'.$autor.'#'.$data;
+        }
+    }
+    else{
+        echo 0;
+    }
+?>
---
+++ Servidor/selectLastPergunta.php
@@ -0,0 +1,26 @@
+<?php
+    include("conexao.php");
+
+    $select = "SELECT * FROM pergunta ORDER BY id DESC LIMIT 1";
+    
+    $result = $conexao->prepare($select);
+    $result->execute();
+    $count = $result->rowCount(); 
+
+    if($count > 0) {
+        echo 1;
+        foreach($result as $perguntas) {
+            $id = $perguntas['id'];
+            $questao = $perguntas['questao'];
+            $resposta = $perguntas['resposta'];
+            $dificuldade = $perguntas['dificuldade'];
+            $tipo = $perguntas['tipo'];
+            $autor = $perguntas['autor'];
+
+            echo '&id='.$id.'#'.$questao.'#'.$resposta.'#'.$dificuldade.'#'.$tipo.'#'.$autor;
+        }
+    }
+    else{
+        echo 0;
+    }
+?>
